e97343881e89d26d0c7486e646fb59d7198e9683
unknown <gonza.edu07@gmail.com>
2023-08-23 19:04:49 -0300

    gonza.edu07@gmail.com

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hola Coder! Soy Gonzalo Miranda!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="timeroman" />
     </View>
   );
 }
