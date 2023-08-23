cd099aa513990f334d76370a3f693541add239bb
unknown <gonza.edu07@gmail.com>
2023-08-23 19:41:19 -0300

    gonza.edu07@gmail.com

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+});
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Coder! Soy Gonzalo Miranda!</Text>
-      <StatusBar style="timeroman" />
+      {/* ...resto del código */}
     </View>
   );
 }
